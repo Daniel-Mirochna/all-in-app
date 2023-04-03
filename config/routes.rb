a56fa9ee97dfb4f0aa3ba702fac1c5aa55cd49c7
Daniel Mirochna <DanielM6@protonmail.com>
2023-04-03 19:38:42 +0200

    Devise routes rename

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', registration: 'account', sign_up: 'register' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
