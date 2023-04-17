3221cdf55c03d527453fb5c1fefcd8dae8a5debb
Daniel Mirochna <daniel.mirochna@ARCHONPLUS.LAN>
2023-04-17 16:17:40 +0200

    Uploads landing page

--- app/controllers/users/uploads_controller.rb
+++ app/controllers/users/uploads_controller.rb
@@ -1,5 +1,17 @@
 class Users::UploadsController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+  end
   
+  def create
+    current_user.files.attach(params[:file])
+    redirect_to root_path
+  end
+
+  def destroy
+    file = current_user.files.find(params[:id])
+    file.purge
+    redirect_to root_path
+  end
 end
