d36e7e3f5eaf98bce397da551d467fa68cab0395
Daniel Mirochna <DanielM6@protonmail.com>
2023-05-03 00:23:02 +0200

    Free storage space counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,19 @@
 class User < ApplicationRecord
   mount_uploaders :user_files, UserFilesUploader
-  validate :check_total_size
+  validate :validate_total_size
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 
-  private
   def check_total_size
     total_size = self.user_files.map(&:size).sum
     max_size = 1073741824 # 1 GB
-    errors.add(:base, "Total size of all files cannot exceed 1 GB") if total_size > max_size
+    total_size.to_f / max_size.to_f
+  end
+
+  private
+  def validate_total_size
+    errors.add(:base, "Total size of all files cannot exceed 1 GB") if self.check_total_size >= 1
   end
 end
