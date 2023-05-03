0b26b644f894406113ca263918c86c34e6321606
Daniel Mirochna <DanielM6@protonmail.com>
2023-05-03 23:51:19 +0200

    Modals for confirmations, not working yet

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Using ESBuild
 import "@hotwired/turbo-rails"
-import "flowbite/dist/flowbite.turbo.js";
+import "flowbite/dist/flowbite.turbo.js"
 import './controllers/index'
+import './popup_modal'
