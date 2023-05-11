b6390d750ba9506affbb079c660d5296c82c9d23
Daniel Mirochna <daniel.mirochna@ARCHONPLUS.LAN>
2023-05-11 16:56:59 +0200

    Start view refactor using helper and presenter

--- app/helpers/crypto_dashboards_helper.rb
+++ app/helpers/crypto_dashboards_helper.rb
@@ -1,2 +1,5 @@
 module CryptoDashboardsHelper
+  def formatted_price(number)
+    number_with_precision(sprintf("%.8f", number), precision: 10, strip_insignificant_zeros: true, delimiter: " ")
+  end
 end
---
+++ app/presenters/crypto_dashboard_presenter.rb
@@ -0,0 +1,3 @@
+class CryptoDashboardPresenter
+  
+end
