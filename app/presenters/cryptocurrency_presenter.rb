fb9bb21e61cd7cd1c3f8be23622f0cff9843fc32
Daniel Mirochna <daniel.mirochna@ARCHONPLUS.LAN>
2023-05-22 16:15:07 +0200

    Change chevron to triangle in displaying 24h % change

--- app/presenters/cryptocurrency_presenter.rb
+++ app/presenters/cryptocurrency_presenter.rb
@@ -16,17 +16,9 @@ class CryptocurrencyPresenter
   def price_change_percentage(number)
     return "-" if number.nil?
     if number.positive?
-      "<span style='display: inline-block; color: rgb(101 163 13);'><svg xmlns='http://www.w3.org/2000/svg'
-        fill='none' viewBox='0 0 24 24' stroke-width='2.5' stroke='currentColor'
-        style='display: inline; width: 10px; height: 10px; vertical-align: initial;'>
-        <path stroke-linecap='round' stroke-linejoin='round' d='M4.5 15.75l7.5-7.5 7.5 7.5' />
-      </svg>&thinsp;#{number.round(2)}&thinsp;%</span>".html_safe
+      "<span class='relative price-change-up' style='display: inline-block; color: rgb(101 163 13); font-weight: 500;'>#{number.round(2)}&thinsp;%</span>".html_safe
     else
-      "<span style='display: inline-block; color: rgb(220 38 38);'><svg xmlns='http://www.w3.org/2000/svg'
-        fill='none' viewBox='0 0 24 24' stroke-width='2.5' stroke='currentColor'
-        style='display: inline; width: 10px; height: 10px; vertical-align: initial;'>
-        <path stroke-linecap='round' stroke-linejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5' />
-      </svg>&thinsp;#{number.round(2).to_s.gsub('-', '')}&thinsp;%</span>".html_safe
+      "<span class='relative price-change-down' style='display: inline-block; color: rgb(220 38 38); font-weight: 500;'>#{number.round(2).to_s.gsub('-', '')}&thinsp;%</span>".html_safe
     end
   end
 end
